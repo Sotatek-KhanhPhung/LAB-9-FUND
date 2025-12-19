d0cd772baf219b08b377edaf0e0f7bd114398334
pnkhanh <pnkhanh@pnkhanhs-MacBook-Pro.local>
2025-12-19 18:31:10 +0700

    Update pub, readme

---
+++ Test Pub : Sub/README.md
@@ -0,0 +1,40 @@
+# File pub.py
+
+```sh
+VIP_HOST = "192.168.215.100"
+VIP_PORT = 5672
+VHOST = "/"
+
+USER = "test_user"     
+PASS = "abc123"
+
+QUEUE = "q.repl_test"
+SEND_INTERVAL_SEC = 1.0
+```
+
+**VIP_HOST:** địa chỉ VIP của Keepalived
+**VIP_PORT:** Port để truy cập vào Queue
+**VHOST, USER, PASS:** thông tin user RabbitMQ
+**QUEUE:** queue sẽ send message đến
+**SEND_INTERVAL_SEC:** khoảng thời gian giữa các lần gửi
+
+```sh
+def connect():
+    return pika.BlockingConnection(
+        pika.ConnectionParameters(
+            host=VIP_HOST,
+            port=VIP_PORT,
+            virtual_host=VHOST,
+            credentials=pika.PlainCredentials(USER, PASS),
+            heartbeat=30,
+            blocked_connection_timeout=30,
+            connection_attempts=1,
+            retry_delay=0,
+            client_properties={"connection_name":"pub-1s-repl-test"},
+        )
+    )
+```
+
+**heartbeat:** phát hiện kết nối TCP bị đứt. Nếu trong 30 giây vẫn có publish, consume, ack, không cần gửi heartbeat. Nếu trong 30 giây không có gì sẽ gửi heartbeat, client và broker mỗi bên sẽ gửi một heartbeat frame
+**blocked_connection_timeout:** thời gian tối đa client chịu connection bị broker block. Nếu sau thời gian này, connection bị đóng và sẽ reconnect
+**connection_attempts, retry_delay:** kiểm soát reconnect
--- Test Pub : Sub/pub.py
+++ Test Pub : Sub/pub.py
@@ -1,91 +1,3 @@
-# #!/usr/bin/env python3
-# import json
-# import time
-# import uuid
-# import pika
-# from pika.exceptions import AMQPConnectionError, StreamLostError
-
-# VIP_HOST = "192.168.215.100"
-# VIP_PORT = 5672
-# VHOST = "/"
-
-# USER = "test_user"
-# PASS = "abc123"
-
-# QUEUE = "q.repl_test"
-# SEND_INTERVAL_SEC = 1.0  # gửi mỗi 1 giây
-
-# def connect():
-#     params = pika.ConnectionParameters(
-#         host=VIP_HOST,
-#         port=VIP_PORT,
-#         virtual_host=VHOST,
-#         credentials=pika.PlainCredentials(USER, PASS),
-#         heartbeat=30,
-#         blocked_connection_timeout=30,
-#         connection_attempts=1,  # tự retry ở vòng while
-#         retry_delay=0,
-#         client_properties={"connection_name": "pub-1s-test"},
-#     )
-#     return pika.BlockingConnection(params)
-
-# def main():
-#     conn = None
-#     ch = None
-#     seq = 0
-
-#     while True:
-#         try:
-#             if conn is None or conn.is_closed or ch is None or ch.is_closed:
-#                 if conn:
-#                     try: conn.close()
-#                     except: pass
-#                 print("[PUB] connecting...")
-#                 conn = connect()
-#                 ch = conn.channel()
-#                 ch.queue_declare(queue=QUEUE, durable=True)
-#                 print("[PUB] connected")
-
-#             seq += 1
-#             msg_id = str(uuid.uuid4())
-#             send_ts_ms = int(time.time() * 1000)
-
-#             payload = {
-#                 "id": msg_id,
-#                 "seq": seq,
-#                 "send_ts_ms": send_ts_ms
-#             }
-
-#             ch.basic_publish(
-#                 exchange="",
-#                 routing_key=QUEUE,
-#                 body=json.dumps(payload).encode("utf-8"),
-#                 properties=pika.BasicProperties(
-#                     content_type="application/json",
-#                     delivery_mode=2,  # persistent
-#                     message_id=msg_id,
-#                     timestamp=int(time.time()),
-#                 ),
-#             )
-
-#             print(f"[PUB] sent seq={seq} id={msg_id} ts_ms={send_ts_ms}")
-#             time.sleep(SEND_INTERVAL_SEC)
-
-#         except (AMQPConnectionError, StreamLostError, ConnectionResetError, BrokenPipeError) as e:
-#             print(f"[PUB] connection error: {type(e).__name__}: {e} -> reconnecting")
-#             conn = None
-#             ch = None
-#             time.sleep(0.5)
-#         except KeyboardInterrupt:
-#             print("\n[PUB] exit")
-#             try:
-#                 if conn: conn.close()
-#             except: pass
-#             return
-
-# if __name__ == "__main__":
-#     main()
-
 
 #!/usr/bin/env python3
 import json
@@ -134,7 +46,6 @@ def main():
                 conn = connect()
                 ch = conn.channel()
 
-                # KHÔNG queue_declare ở đây để tránh mismatch arguments quorum/classic
                 print(f"[PUB] connected -> publishing to queue={QUEUE} via VIP {VIP_HOST}:{VIP_PORT}")
 
             seq += 1
